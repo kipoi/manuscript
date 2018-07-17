c14e5e69572e2d278bdacf830d4df994dffafb5f
Avsecz <zigaavsec@gmail.com>
2018-07-17 15:04:24 +0200

    updated the deepbind file-paths

--- m_kipoi/exp/tfbinding/config.py
+++ m_kipoi/exp/tfbinding/config.py
@@ -20,28 +20,28 @@ TF2CT = {tf: ct for tf, ct in TF_C_pairs}
 SINGLE_TASK_MODELS = {
     "CEBPB": {
         "pwm_HOCOMOCO": "pwm_HOCOMOCO/human/CEBPB",
-        "DeepBind": "DeepBind/D00317.009",
+        "DeepBind": "DeepBind/Homo_sapiens/TF/D00317.009_ChIP-seq_CEBPB",
         "FactorNet": "FactorNet/CEBPB/meta_Unique35_DGF",
         "DeepSEA": "DeepSEA/predict",
         "lsgkm-SVM": "lsgkm-SVM/Tfbs/Cebpb/Helas3/Sydh_Iggrab",
     },
     "JUND": {
         "pwm_HOCOMOCO": "pwm_HOCOMOCO/human/JUND",
-        "DeepBind": "DeepBind/D00776.005",
+        "DeepBind": "DeepBind/Homo_sapiens/TF/D00776.005_ChIP-seq_JUND",
         "FactorNet": "FactorNet/JUND/meta_Unique35_DGF_2",  # meta_Unique35_DGF_2
         "DeepSEA": "DeepSEA/predict",
         "lsgkm-SVM": "lsgkm-SVM/Tfbs/Jund/Hepg2/Sydh_Iggrab",
     },
     "MAFK": {
         "pwm_HOCOMOCO": "pwm_HOCOMOCO/human/MAFK",
-        "DeepBind": "DeepBind/D00503.014",
+        "DeepBind": "DeepBind/Homo_sapiens/TF/D00503.014_ChIP-seq_MAFK",
         "FactorNet": "FactorNet/MAFK/meta_1_Unique35_DGF",
         "DeepSEA": "DeepSEA/predict",
         "lsgkm-SVM": "lsgkm-SVM/Tfbs/Mafkab50322/K562/Sydh_Iggrab",
     },
     "NANOG": {
         "pwm_HOCOMOCO": "pwm_HOCOMOCO/human/NANOG",
-        "DeepBind": "DeepBind/D00786.001",
+        "DeepBind": "DeepBind/Homo_sapiens/TF/D00786.001_ChIP-seq_NANOG",
         "FactorNet": "FactorNet/NANOG/onePeak_Unique35_DGF",  # GENCODE_Unique35_DGF
         "DeepSEA": "DeepSEA/predict",
         "lsgkm-SVM": "lsgkm-SVM/Tfbs/Nanogsc33759/H1hesc/Haib_V0416102",
